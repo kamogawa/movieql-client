cda09df8405484d6ae26637656a27b5fa4b82bb0
The9 <captain_kbg@naver.com>
2019-03-04 23:48:29 +0900

    Creating Query

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,8 +1,8 @@
-import { injectGlobal } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
-injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Maven+Pro');
-  ${reset};
+createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Maven+Pro:400,500');
+${reset};
   body{
     font-family: 'Maven Pro', sans-serif;
     background-color: #ecf0f1;
